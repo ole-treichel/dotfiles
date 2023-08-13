5fe0283c99b2da61e1623cad19bfbe58886d5544
Ole Treichel <root@ole.md>
2023-08-14 01:05:11 +0200

    Add lsp code action keybind

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -78,6 +78,7 @@ lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
 end)
 
 lsp.setup()
