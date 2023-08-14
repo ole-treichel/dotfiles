34db2413ce93347f0a7163207483d10df8a2831b
Ole Treichel <root@ole.md>
2023-08-14 10:32:46 +0200

    Change keybind for fuzzy find without git

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,5 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ffa', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fa', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ffg', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
