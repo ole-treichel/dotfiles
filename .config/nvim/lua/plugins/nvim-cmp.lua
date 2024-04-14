5cb0d5d89a00d2dbc3f876cf69aa620eaa1175b3
Ole Treichel <root@ole.md>
2024-04-14 13:47:14 +0200

    Add nvim-cmp with lsp configuration

---
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,73 @@
+return {
+  "hrsh7th/nvim-cmp",
+
+  dependencies = {
+    "neovim/nvim-lspconfig",
+
+    "hrsh7th/cmp-nvim-lsp",
+    "L3MON4D3/LuaSnip"
+  },
+
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+
+        -- ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+        -- Supertab with luasnip https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
+        ['<CR>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            if luasnip.expandable() then
+              luasnip.expand()
+            else
+              cmp.confirm({
+                select = true,
+              })
+            end
+          else
+            fallback()
+          end
+        end),
+
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.locally_jumpable(1) then
+            luasnip.jump(1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' }, 
+      }, {
+        { name = 'buffer' },
+      })
+    })
+  end
+}
