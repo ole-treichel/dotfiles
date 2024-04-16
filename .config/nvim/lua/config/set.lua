98b4aa40d8b52727e2b6d5e4a1aaa7112c68c1c3
Ole Treichel <root@ole.md>
2024-04-16 20:36:49 +0200

    Add listchars

--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -2,23 +2,23 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-
+vim.opt.list = true
+vim.opt.listchars = {
+  tab = '⇥ ',
+  leadmultispace = '┊ ',
+  trail = '␣',
+  nbsp = '⍽',
+}
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = 'yes:1'
-
 vim.opt.wrap = false
-
 vim.opt.termguicolors = true
-
 vim.opt.timeoutlen = 300
-
 -- show statusbar only for current file
 vim.opt.laststatus = 3
-
 -- bun hot / watch fix: https://github.com/oven-sh/bun/issues/8520#issuecomment-2002325950
 vim.opt.backupcopy = 'yes'
-
 vim.filetype.add {
   extension = {
     templ = 'templ',
