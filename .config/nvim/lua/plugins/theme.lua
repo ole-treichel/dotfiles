41e47bae7a71e2d72d3707fba05f85542e3fc948
Ole Treichel <root@ole.md>
2024-07-12 23:59:33 +0200

    Formatting

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,7 +1,7 @@
-return { 
-  "rose-pine/neovim",
-  name = "rose-pine",
-  config = function ()
-    vim.cmd("colorscheme rose-pine-main")
-  end
+return {
+  'rose-pine/neovim',
+  name = 'rose-pine',
+  config = function()
+    vim.cmd 'colorscheme rose-pine-main'
+  end,
 }
