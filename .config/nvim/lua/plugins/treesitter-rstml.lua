d3f1bf9cee12aaedd70f20f97f3439ce1b64b796
Ole Treichel <root@ole.md>
2024-09-08 00:28:39 +0200

    Add injection highligting for alpine js attributes inside shtml / rstml

--- .config/nvim/lua/plugins/treesitter-rstml.lua
+++ .config/nvim/lua/plugins/treesitter-rstml.lua
@@ -4,5 +4,27 @@ return {
   build = ':TSUpdate',
   config = function()
     require('tree-sitter-rstml').setup()
+
+    vim.treesitter.query.set("rust_with_rstml", "injections", [[
+      (
+        (node_attribute
+          name: (node_identifier) @_attr_name
+          value: (rust_expression (string_literal (string_content) @injection.content))
+        )
+        (#match? @_attr_name "(^(x-data|x-init|x-for|x-if|x-effect|x-show)$|(^x-on:|^x-bind:|^x-text:))")
+        (#set! injection.include-children)
+        (#set! injection.combined)
+        (#set! injection.language "javascript")
+      )
+    ]])
+
+    -- rust analyzers semantic tokens overwrite the injections highlighting. this disables semantic tokens entirely
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+      callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        client.server_capabilities.semanticTokensProvider = nil
+      end,
+    })
   end,
 }
