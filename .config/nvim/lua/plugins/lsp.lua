cb1c8515c5204fe07c3c50fd997c7bf5115c08ae
Ole Treichel <root@ole.md>
2024-12-15 13:39:21 +0100

    Add missing lsp keymaps for references and rename

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -48,6 +48,8 @@ return {
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
         vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', 'grr', vim.lsp.buf.references)
+        vim.keymap.set('n', 'grn', vim.lsp.buf.rename)
       end,
     })
   end,
