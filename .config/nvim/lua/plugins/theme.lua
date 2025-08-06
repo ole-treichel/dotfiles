8d7c59f40330c28461c88504c79e295d8c3abc67
Ole Treichel <root@ole.md>
2025-08-06 14:10:27 +0200

    Theme switch

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,7 +1,18 @@
 return {
-  'rose-pine/neovim',
-  name = 'rose-pine',
+  'ole-treichel/poimandres.nvim',
+  lazy = false,
+  priority = 1000,
   config = function()
-    vim.cmd 'colorscheme rose-pine'
+    require('poimandres').setup {
+      -- leave this setup function empty for default config
+      -- or refer to the configuration section
+      -- for configuration options
+    }
   end,
+
+  -- optionally set the colorscheme within lazy config
+  init = function()
+    vim.cmd("colorscheme poimandres")
+  end
 }
+
