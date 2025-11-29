479e687465fa7aff8979f9cb521f92644f671388
Ole Treichel <root@ole.md>
2025-11-29 17:49:26 +0100

    Powerline style

--- .config/nvim/lua/plugins/tiny-inline-diagnostics.lua
+++ .config/nvim/lua/plugins/tiny-inline-diagnostics.lua
@@ -3,7 +3,9 @@ return {
     event = "VeryLazy",
     priority = 1000,
     config = function()
-        require("tiny-inline-diagnostic").setup()
+        require("tiny-inline-diagnostic").setup({
+            preset = "powerline"
+        })
         vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
     end,
 }
