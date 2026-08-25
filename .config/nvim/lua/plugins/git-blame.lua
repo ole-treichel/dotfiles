16444d8448795946c2a54d42120a22e8c69eaeb0
Ole Treichel <root@ole.md>
2026-08-25 09:58:33 +0200

    Fix overlaying issues between lsp and gitblame

--- .config/nvim/lua/plugins/git-blame.lua
+++ .config/nvim/lua/plugins/git-blame.lua
@@ -13,6 +13,9 @@ return {
         message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
         date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
         virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
+        -- keep blame text below tiny-inline-diagnostic's virtual text (priority 2048)
+        -- so error hints always win when both land on the same line/column
+        set_extmark_options = { priority = 100 },
     },
  
 }
